98b1aa8e5e2cb2e8a7d7c261003c62af23024618
Roni <roni.c.castro@gmail.com>
2022-05-19 01:15:23 -0300

    feat: config axios on web

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import api from '@monorepo/axios-config';
 
 const App = () => {
+  api.get('https://gorest.co.in/public/v2/users').then(console.log);
   return <h1>Hello World</h1>;
 };
 
